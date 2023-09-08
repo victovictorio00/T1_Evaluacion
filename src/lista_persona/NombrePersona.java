9c4085a3e76f5e1468d8cf2ac859a8bc9d1c7754
victovictorio00 <142622750+victovictorio00@users.noreply.github.com>
2023-09-08 13:44:35 -0500

    Add files via upload

---
+++ src/lista_persona/NombrePersona.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lista_persona;
+
+/**
+ *
+ * @author agost
+ */
+
+public class NombrePersona {
+        private String nombre;
+
+    public NombrePersona(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
